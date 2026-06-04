6b10dbc5701804ef9fb26e1bf5fc17b5da8c99cf
Daniel Meden Mejias <75368881+Danielmm2001@users.noreply.github.com>
2026-06-04 18:24:07 +0200

    Add Flutter API constants

---
+++ app/lib/core/constants/api_constants.dart
@@ -0,0 +1,9 @@
+class ApiConstants {
+  // Android emulator uses 10.0.2.2 to reach the host machine.
+  // Real Android device on same WiFi should use your laptop LAN IP, for example:
+  // static const baseUrl = 'http://192.168.1.141:8000';
+  static const baseUrl = 'http://10.0.2.2:8000';
+
+  // Temporary user id until Supabase Auth is connected in Flutter.
+  static const demoUserId = 'e49e5c2b-422a-48ed-b579-beaa735abe44';
+}
